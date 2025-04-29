b1f44dea93a83bb7797297177e1f245142afd53b
Najmunda <najmundaakmal@gmail.com>
2025-04-29 23:25:13 +0700

    feat: Add error & success sync toast to /sync action

--- src/routes/sync.jsx
+++ src/routes/sync.jsx
@@ -1,6 +1,15 @@
+import toast from "react-hot-toast";
 import { syncDB } from "../db";
+import Toast from "../components/Toast";
 
 export async function action() {
-  const response = await syncDB();
-  return response;
+  try {
+    const response = await syncDB();
+    toast.custom((t) => (<Toast message="Kartu berhasil disinkronisasi." color="green" />));
+    return response;
+  } catch (error) {
+    console.log("error:", error);
+    toast.custom((t) => (<Toast message="Terjadi galat saat sinkronisasi. Ulangi." color="red" />));
+    return null;
+  }
 }
