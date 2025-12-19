127851a5631109a09056b905518eb7797d402f06
Najmunda <najmundaakmal@gmail.com>
2025-12-19 19:40:17 +0700

    feat: put dialog on bottom of mobile/sm screen

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -6,11 +6,11 @@ const Dialog = forwardRef(function Dialog({ children, ...props }, ref) {
     <dialog
       ref={ref}
       {...props}
-      className="h-screen w-screen absolute not-open:hidden open:flex justify-center items-center top-0 bg-black/50 z-50"
+      className="h-dvh w-dvw not-open:hidden open:flex justify-center items-end sm:items-center bg-black/50 z-50"
     >
       <Card
         as="div"
-        className="w-full h-fit max-h-[75dvh] sm:max-w-sm md:max-w-md flex flex-col items-stretch rounded-lg overflow-hidden"
+        className="w-full h-fit max-h-[75dvh] sm:max-w-sm md:max-w-md flex flex-col items-stretch rounded-b-none rounded-t-lg sm:rounded-lg overflow-hidden"
       >
         <div className="flex-1 py-[2px] max-h-[calc(75dvh-16px)] overflow-auto scrollbar-thin flex flex-col gap-2">
           {children}
