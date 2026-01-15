7abd5b57722325e61bfdee7ed34e3323bfd7a709
Najmunda <najmundaakmal@gmail.com>
2026-01-15 22:25:33 +0700

    feat: add compactDB to /cards delete

--- src/routes/cards/delete.jsx
+++ src/routes/cards/delete.jsx
@@ -2,20 +2,21 @@ import toast from "react-hot-toast";
 import Toast from "../../components/Toast";
 import { logError } from "../../utils/logger";
 import { deleteCardDoc } from "./db";
+import { compactDB } from "../../db";
 
 export async function action({ params }) {
-  let success;
+  let success = false;
   try {
     const response = await deleteCardDoc(params.cardId);
     toast.custom(() => <Toast message={response.message} type="success" />);
     success = true;
+    await compactDB();
   } catch (error) {
     await logError(error);
     error.message = Object.hasOwn(error, "cause")
       ? error.message
       : "Kartu gagal dihapus";
     toast.custom(() => <Toast message={error.message} type="error" />);
-    success = false;
   }
   return { success };
 }
